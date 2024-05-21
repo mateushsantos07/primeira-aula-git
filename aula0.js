1aaf3a7ba5006c35bf2ee8873f2490a0e6c2fed8
MATEUS HENRIQUE DOS SANTOS <mateus_santos6@estudante.sesisenai.org.br>
2024-05-21 20:54:39 -0300

    que barbaridade

---
+++ aula0.js
@@ -0,0 +1,36 @@
+var media = (1 + 2 + 2) / 3
+
+console.log(media.toFixed(2))
+
+console.log(media)
+
+console.log("Ola Mundo")
+console.log("Salva antes")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
